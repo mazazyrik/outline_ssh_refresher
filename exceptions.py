052e6ed6c7644ff861397e80e1af6b7736e4cec8
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2024-03-29 16:07:17 +0300

    admin added but to test you should turn off prod

--- exceptions.py
+++
@@ -1,2 +0,0 @@
-class UserAlreadyHasKey(Exception):
-    pass
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 from outline_vpn import OutlineVPN
 
-client = OutlineVPN(api_url="https://87.247.142.222:63164/CtykuO9_lRq6hgdCNjeFow",
-                    cert_sha256="8D876891BEE756FC2CF72E0683EA99807A860974F6976951D3942DFE7226BC9D")
+client = OutlineVPN(api_url=(
+    "https://87.247.142.222:63164/CtykuO9_lRq6hgdCNjeFow"
+),
+    cert_sha256=(
+    "8D876891BEE756FC2CF72E0683EA99807A860974F6976951D3942DFE7226BC9D"
+))
 
 
 def get_new_key(name):
@@ -17,3 +21,18 @@ def delete_all_keys():
 
 def get_all_keys():
     return [key.name for key in client.get_keys()]
+
+
+def delete_key(name):
+    for key in client.get_keys():
+        if key.name == name:
+            client.delete_key(key.key_id)
+            return True
+
+
+
+def all_keys_str():
+    all_keys = ''
+    for name in get_all_keys():
+        all_keys += f'{name}, '
+    return all_keys
